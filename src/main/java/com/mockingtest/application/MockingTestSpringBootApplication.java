a50ab8272f45e7b41f3b21a698fdb57f90697a1d
Xinge Wen <xinge.wensxwj@gmail.com>
2018-06-19 01:26:48 -0700

    build success

--- src/main/java/com/mockingtest/application/MockingTestSpringBootApplication.java
+++ src/main/java/com/mockingtest/application/MockingTestSpringBootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebApplicationBootstrapJqueryApplication {
+public class MockingTestSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
--- src/test/java/com/mockingtest/application/MockingTestSpringBootApplicationTests.java
+++ src/test/java/com/mockingtest/application/MockingTestSpringBootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootWebApplicationBootstrapJqueryApplicationTests {
+public class MockingTestSpringBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
